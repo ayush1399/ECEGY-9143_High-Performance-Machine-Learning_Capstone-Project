dd83176cc293ae39a19c19e8b849fcabd7fcb2b3
Ayushman Singh <git@ayushmansingh.com>
2023-11-26 08:23:59 -0500

    rename models

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,9 @@
 from torchvision.models import resnet152 as ResNet
 
-__all__ = ["ResNet"]
+import ViT
+import RPN
+import RPN_P
+import RPN_PQ
+import RPN_PQ_EE
+
+__all__ = ["ResNet", "RPN", "RPN_P", "RPN_PQ", "RPN_PQ_EE", "ViT"]
