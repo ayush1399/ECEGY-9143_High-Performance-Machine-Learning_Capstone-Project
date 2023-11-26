ac8e50845e48dc7b365878ba23048a5f269fb936
Ayushman Singh <git@ayushmansingh.com>
2023-11-26 05:27:30 -0500

    add models/__init__.py

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+from torchvision.models import resnet152
+
+models = {
+    "RPN": None,
+    "RPN-P": None,
+    "RPN-PQ": None,
+    "RPN-PQ-EE": None,
+    "ViT": None,
+    "ResNet": resnet152,
+}
