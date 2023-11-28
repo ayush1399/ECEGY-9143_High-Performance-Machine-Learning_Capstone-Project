d2069ad8600b35a17c126dc3205358cdcdb735ca
Ayushman Singh <git@ayushmansingh.com>
2023-11-28 04:44:59 -0500

    add hacky solution for models module resolution

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,10 @@
 from torchvision.models import resnet152, ResNet152_Weights
 
+import sys
+import os
+
+sys.path.append(os.path.join(os.getcwd(), "models"))
+
 
 def ResNet(weights=ResNet152_Weights.IMAGENET1K_V1, progress=False, **kwargs):
     return resnet152(weights=weights, progress=progress, **kwargs)
