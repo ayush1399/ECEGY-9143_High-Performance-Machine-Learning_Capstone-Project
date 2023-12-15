74e2e766b351acdd840c941e6411ad5e343e263b
Ayushman Singh <git@ayushmansingh.com>
2023-12-14 20:50:18 -0500

    update models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,9 @@
-from torchvision.models import resnet152
+import torchvision.models as M
 
-"""
-from torchvision.models import (
-    resnet152,
-    ResNet152_Weights,
-    vit_b_32,
-    ViT_B_32_Weights,
-    swin_v2_b,
-    Swin_V2_B_Weights,
-)
-"""
+import RPN
+import RPN_P
+import RPN_PQ
+import RPN_PQ_EE
 
 import sys
 import os
@@ -17,26 +11,28 @@ import os
 sys.path.append(os.path.join(os.getcwd(), "models"))
 
 
-def ResNet(pretrained=True):
-    return resnet152(weights=weights, pretrained=True)
+def get_model_weights(model, version):
+    weights = getattr(M, model)
+    return getattr(weights, version)
 
 
-"""
-import ViT
-from RPN import RPN
-=======
-def ViT(weights=ViT_B_32_Weights.IMAGENET1K_V1, progress=False, **kwargs):
-    return vit_b_32(weights=weights, progress=progress, **kwargs)
+def ResNet(
+    weights="ResNet152_Weights", version="IMAGENET1K_V1", progress=False, **kwargs
+):
+    weights = get_model_weights(weights, version)
+    return M.resnet152(weights=weights, progress=progress, **kwargs)
 
 
-def Swin_V2(weights=Swin_V2_B_Weights.IMAGENET1K_V1, progress=False, **kwargs):
-    return swin_v2_b(weights=weights, progress=progress, **kwargs)
+def ViT(weights="ViT_B_32_Weights", version="IMAGENET1K_V1", progress=False, **kwargs):
+    weights = get_model_weights(weights, version)
+    return M.vit_b_32(weights=weights, progress=progress, **kwargs)
 
-"""
-import RPN
 
-import RPN_P
-import RPN_PQ
-import RPN_PQ_EE
+def Swin_V2(
+    weights="Swin_V2_B_Weights", version="IMAGENET1K_V1", progress=False, **kwargs
+):
+    weights = get_model_weights(weights, version)
+    return M.swin_v2_b(weights=weights, progress=progress, **kwargs)
 
-__all__ = ["ResNet", "RPN", "RPN_P", "RPN_PQ", "RPN_PQ_EE"]
+
+__all__ = ["ResNet", "RPN", "RPN_P", "RPN_PQ", "RPN_PQ_EE", "ViT", "Swin_V2"]
